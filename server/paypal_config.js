d68489ce4b988f34edb90f628bbb7659d8b19e25
momen-25 <momenashraf25@students.mans.edu.eg>
2020-07-14 13:26:07 +0200

    My first commit!

---
+++ server/paypal_config.js
@@ -0,0 +1,7 @@
+Meteor.Paypal.config({
+    'host':'https://api.sandbox.paypal.com',
+    'port':'',
+    'client_id':'ARIp2ko_K0cgYn3qON-DBcJnah6rkcxDm26iDpbzD8uSie0U7bu3lYK1uUsqr-Ei6Sw5_qrqi62ldSzu',
+    'client_secret':'EMEoNZOp6VBGyGBC2NflfQs34jWvjktx_ZCh_WGh_A5y_t9a7fwuhNAwYi3P-ZZtCIx1_abctwwXcp6H'
+});
+console.log("asasassa")
