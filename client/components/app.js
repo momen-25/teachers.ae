264e13c1bfd4e15c41e5a8c083a12ea6448548a8
momen-25 <momenashraf25@students.mans.edu.eg>
2020-08-03 23:08:32 +0200

    sec commit

--- client/components/app.js
+++ client/components/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Car from './header'
 
 export default () => {
 	return (
      <div>
        momen
+       <Car />
+
      </div>
     )
 }
---
+++ client/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Car() {
+   console.log("fuck");	
+    return (
+     <nav>
+     I am a Car
+     </nav>
+     );
+  }
+
+
+export default Car
